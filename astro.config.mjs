1e282508da02b003da328fea4cb6510fca3f690d
cyflu <cyflu@DESKTOP-NAHKO36>
2024-12-03 12:49:33 +0900

    init i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,24 +5,41 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site:"https://github.com/file-name-system/file-name-system.github.io",
-	
+	trailingSlash:"always",
+
 	integrations: [
 		starlight({
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/custom.css',
+			  ],
 			title: 'File Name System',
+			defaultLocale: 'en', // optional
+			locales: {
+				en: {
+					label: 'English',
+				  },
+				'ko': {
+				  label: '한국어',
+			
+				}
+			},
+			components: {
+				// Override the default `SocialIcons` component.
+				//PageFrame: './src/components/PageFrame.astro',
+				//Header: './src/components/Header.astro',
+			  },
 			social: {
 				github: 'https://github.com/file-name-system/file-name-system.github.io',
 			},
 			sidebar: [
 				{
 					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+				 slug: 'guide'
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Spec',
+				 slug: 'spec'
 				},
 			],
 		}),
